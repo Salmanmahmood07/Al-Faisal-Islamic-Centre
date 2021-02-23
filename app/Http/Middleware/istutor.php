138efbc3ab393effb755618f9fcb7455cd7928d3
salman mahmood <salman.mahmood24@gmail.com>
2021-02-23 19:15:53 +0500

    start working on tutor

---
+++ app/Http/Middleware/istutor.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class istutor
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::check()){
+          if(Auth::user()->type == 'tutor'){
+              return $next($request);
+          }
+      }
+       Auth::logout();
+        return redirect('/tutor');
+    }
+}
